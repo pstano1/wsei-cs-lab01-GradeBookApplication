ea77d2bfdffb13d38b471743e1675a9e70a7ab4a
pstano1 <p.stano@outlook.com>
2023-04-02 15:22:40 +0200

    feat: override RankedGradeBook's GetLetterGrade method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,40 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked grading requires at least 5 students.");
+            }
+
+            int rankThreshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(s => s.AverageGrade)
+                                .Select(s => s.AverageGrade)
+                                .ToList();
+
+            if (grades[rankThreshold - 1] <= averageGrade)
+            {
+                return 'A';
+            }
+            else if (grades[(rankThreshold * 2) - 1] <= averageGrade)
+            {
+                return 'B';
+            }
+            else if (grades[(rankThreshold * 3) - 1] <= averageGrade)
+            {
+                return 'C';
+            }
+            else if (grades[(rankThreshold * 4) - 1] <= averageGrade)
+            {
+                return 'D';
+            }
+            else
+            {
+                return 'F';
+            }
+        }
+
     }
 }
