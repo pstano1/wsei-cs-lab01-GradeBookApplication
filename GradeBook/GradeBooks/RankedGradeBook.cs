9f3d4e4e0506910c8807b2a7a3064cec2eded3bb
pstano1 <p.stano@outlook.com>
2023-04-02 15:17:37 +0200

    feat: creating the RankedGradeBook class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using GradeBook.Enums;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(string name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
